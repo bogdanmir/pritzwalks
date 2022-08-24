0d3ee7275f51354f4fe34bd2e6f8fdfb565245ef
bogdanmir <muronhtz@gmail.com>
2022-08-24 17:22:30 +0300

    update

---
+++ assets/Contact.3fd1f796.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o,e as s}from"./index.fec45bf6.js";const c={},n={class:"flex justify-center lg:pt-80 pt-60 pb-52"},a=s("h2",{class:"font-mainbold text-4xl"},"Coming soon...",-1),_=[a];function r(i,l){return o(),e("div",n,_)}const f=t(c,[["render",r]]);export{f as default};
---
+++ assets/Faq.e04eca1c.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,e as s}from"./index.fec45bf6.js";const c={},n={class:"flex justify-center lg:pt-80 pt-60 pb-52"},a=s("h2",{class:"font-mainbold text-4xl"},"Coming soon...",-1),_=[a];function r(i,l){return o(),t("div",n,_)}const f=e(c,[["render",r]]);export{f as default};
