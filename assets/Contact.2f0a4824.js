2fe1f3b6e0153b21dcf1ee1510f594a845cefee7
bogdanmir <muronhtz@gmail.com>
2022-08-24 17:05:16 +0300

    update

---
+++ assets/Contact.2f0a4824.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o,a as s}from"./index.d7005884.js";const c={},n={class:"flex justify-center lg:pt-80 pt-60 pb-52"},a=s("h2",{class:"font-mainbold text-4xl"},"Coming soon...",-1),_=[a];function r(i,l){return o(),e("div",n,_)}const f=t(c,[["render",r]]);export{f as default};
